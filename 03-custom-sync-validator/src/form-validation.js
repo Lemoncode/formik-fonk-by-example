a5dc1bcd2ac27285fa3cc617c0fd0371d141ad9d
Braulio Diez Botella <braulio.diez@gmail.com>
2019-12-01 14:29:43 +0100

    Example 03 completed

---
+++ 03-custom-sync-validator/src/form-validation.js
@@ -0,0 +1,47 @@
+import { createFinalFormValidation } from '@lemoncode/fonk-final-form';
+import { Validators } from '@lemoncode/fonk';
+import { iban } from '@lemoncode/fonk-iban-validator';
+import { rangeNumber } from '@lemoncode/fonk-range-number-validator';
+
+export const validationSchema = {
+  field: {
+    account: [Validators.required, iban],
+    name: [Validators.required],
+    integerAmount: [
+      Validators.required,
+      {
+        validator: rangeNumber,
+        customArgs: {
+          min: {
+            value: 0,
+            inclusive: true,
+          },
+          max: {
+            value: 10000,
+            inclusive: true,
+          },
+        },
+      },
+    ],
+    decimalAmount: [
+      Validators.required,
+      {
+        validator: rangeNumber.validator,
+        customArgs: {
+          min: {
+            value: 0,
+            inclusive: true,
+          },
+          max: {
+            value: 99,
+            inclusive: true,
+          },
+        },
+      },
+    ],
+    reference: [Validators.required],
+    email: [Validators.required, Validators.email],
+  },
+};
+
+export const formValidation = createFinalFormValidation(validationSchema);
